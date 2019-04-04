18aa667af73b8211d38a54f542227cee41014d00
j0sh1x <aljoshua.hell@gmail.com>
2019-04-04 21:43:48 +0200

    edit build script to put out a finished img

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 export CROSS_COMPILE=/home/j0sh1x/toolchain/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/j0sh1x/toolchain32/bin/arm-linux-androideabi-
 export ARCH=arm64
-#path="/home/j0sh1x/clang-toolchain/clang-r353983/bin/"
-#export DATE=date '+%d-%m-%Y-%H-%M-%S'
+d=`date +%m%d`
+path="/home/j0sh1x/clang-toolchain/clang-r353983/bin/"
 defcon=b1c1_defconfig
+dp=DP2
 
 if [ -d out/ ]
 then
@@ -24,4 +25,8 @@ then
     mkdir ../kernels/
 fi
 
-cp /home/j0sh1x/kernel-crosshatch-q-dp2/out/arch/arm64/boot/Image.lz4-dtb ../kernels/Image.lz4-dtb
+./scripts/mkbootimg/mkbootimg.py --kernel out/arch/arm64/boot/Image.lz4-dtb --ramdisk scripts/prebuilt/ramdisk --dtb scripts/prebuilt/dtb --cmdline 'console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on msm_rtb.filter=0x237 ehci-hcd.park=3 service_locator.enable=1 cgroup.memory=nokmem lpm_levels.sleep_disabled=1 usbcore.autosuspend=7 loop.max_part=7 androidboot.boot_devices=soc/1d84000.ufshc androidboot.super_partition=system buildvariant=userdebug androidboot.selinux=permissive' --header_version 2 -o ../kernels/${d}_KernelSU_${dp}.img
+
+echo "Your finished kernel is in ../kernels"
+
+
